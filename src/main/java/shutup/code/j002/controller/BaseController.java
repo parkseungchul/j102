e5424da10ff180e898bf9fb0b00d45e59452d59f
simon <parkseungchul@gmail.com>
2020-11-11 13:41:18 +0900

    j102

--- src/main/java/shutup/code/j002/controller/BaseController.java
+++ src/main/java/shutup/code/j002/controller/BaseController.java
@@ -1,5 +1,10 @@
 package shutup.code.j002.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +13,19 @@ public class BaseController {
 	
 	@RequestMapping("/")
 	public String main() {
+		
+		InetAddress local; 
+		SimpleDateFormat format2 = new SimpleDateFormat ( "yyyy년 MM월dd일 HH시mm분ss초");
+		Date time = new Date();
+		String time2 = format2.format(time);
+
+		try { 
+			local = InetAddress.getLocalHost(); 
+			String ip = local.getHostAddress(); 
+			System.out.println("Call Time: " + time2 + ", " + "Call IP: " + ip); 
+			} catch (UnknownHostException e1) { 
+				e1.printStackTrace(); 
+		}
 		return "index";
 	}
 
